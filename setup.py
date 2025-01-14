753778f61c8d3943dddc1fd1984a16f0d249c401
Noufel <148371707+Nouf-Nouf@users.noreply.github.com>
2025-01-14 15:18:02 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+name='{Quizz}',
+version='1.0',
+author='{Nouf-Nouf}',
+license='MIT',
+long_description=open('README.md').read(),
+)
